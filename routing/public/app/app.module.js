880c7ed854d153b6102044225eed84444b9f22d3
jessehancock <jessehancock1@gmail.com>
2017-01-07 11:43:54 -0700

    adding routing

---
+++ routing/public/app/app.module.js
@@ -0,0 +1,22 @@
+angular.module("app", ['ui-router'])
+.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	.state('home', {
+    url: '/',
+    templateUrl: './public/views/home.html'
+   })
+  .state('about', {
+    url: '/about',
+    templateUrl: './public/views/about.html'
+   })
+   .state('detox', {
+    url: '/detox',
+    templateUrl: './public/views/detox.html'
+   })
+  //  .state('registration', {
+  //   url: '/registration',
+  //   templateUrl: './views/registration.html'
+  //  })
+})
